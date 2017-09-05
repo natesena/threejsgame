53952e3fafb395194d967d1bd844fb742656b3fa
Nathaniel Sena <nate.sena1@gmail.com>
2017-09-05 14:50:00 -0700

    added text functionality

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ document.body.addEventListener( 'keydown', onKeyDown, false );
 
 var spawns = [];
 var bullets = [];
+var strings = [];
+
+var score = 0;
 
 //-----------GUI Elements--------------------
 var loader = new THREE.TextureLoader();
@@ -24,6 +27,13 @@ scene.add( camera );
 // ambient light to see floor
 scene.add( new THREE.AmbientLight( 0x666666 ) );
 
+//skybox
+// var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+// var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
+// var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+// scene.add(skyBox);
+// scene.fog = new THREE.FogExp2( 0x9999ff, 0.00025 );
+
 //floor
 var groundTexture = loader.load( 'img/grasslight-big.jpg' );
 groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
@@ -35,6 +45,27 @@ mesh.position.y = - 5;
 mesh.rotation.x = - Math.PI / 2;
 scene.add( mesh );
 
+var loader = new THREE.FontLoader();
+
+function updateText(string){
+    scene.remove(strings[0]);
+    strings.shift();
+    loader.load( 'fonts/font.json', function ( font ) {
+            var textGeo = new THREE.TextGeometry( string, {
+                font: font,
+                size: .25,
+                height: .05,
+                curveSegments: 12,
+            } );
+            var textMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+            textMesh = new THREE.Mesh( textGeo, textMaterial );
+            textMesh.position.set(camera.position.x-5*Math.sin(camera.rotation.y), 3, camera.position.z-5*Math.cos(camera.rotation.y));
+            textMesh.rotation.set(0, camera.rotation.y, 0);
+            scene.add(textMesh);
+            strings.push(textMesh);
+        } );
+        
+}
 
 //object functions
 function ranPos(x, y, z){
@@ -57,34 +88,58 @@ function randomColor(){
 
 function spawnBox() {
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: randomColor() } );
-    var cube = new THREE.Mesh( geometry, material );
-    var posArr = ranPos(2, 2, 2);
+    var wireframeMaterial = new THREE.MeshBasicMaterial( { color: randomColor(), wireframe: true, transparent: true } ); 
+    var cube = new THREE.Mesh( geometry, wireframeMaterial );
+    var posArr = ranPos(1, 1, 1);
     spawns.push(cube);
     scene.add( cube );
     cube.position.set(posArr[0],posArr[1],posArr[2])
 }
 function shoot(){
-    var geometry = new THREE.SphereGeometry( .05, 8, 8 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    var bullet = new THREE.Mesh( geometry, material );
-    bullet.position.set(camera.position.x, camera.position.y, camera.position.z - 1);
+    var geometry = new THREE.SphereGeometry( .5, 8, 8 );
+    var wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ee00, wireframe: true, transparent: true } ); 
+    var bullet = new THREE.Mesh( geometry, wireframeMaterial );
+    bullet.position.set(camera.position.x, camera.position.y, camera.position.z);
+    bullet.velocity = new THREE.Vector3(
+        -Math.sin(camera.rotation.y),
+        0,//change this to y velocity minus gravity
+        -Math.cos(camera.rotation.y)
+    );
+    bullet.alive = true;
+    setTimeout(function(){
+        bullet.alive = false;
+        scene.remove(bullet);
+        bullets.shift();
+    }, 1000);
     bullets.push(bullet);
     scene.add(bullet);
-    console.log(camera.position);
+    console.log(bullets.length);
     
 }
+
+// function detectCollision(object){
+//     for(var i = 0; i < spawns.length; i++){
+//         var distance = ((object.position.x-spawns[i].position.x)^2+(object.position.y-spawns[i].position.y)^2+(object.position.z-spawns[i].position.z)^2)^.5;
+//         console.log('xDistance: '+(object.position.x-spawns[i].position.x)^2);
+//         console.log('yDistance: '+(object.position.y-spawns[i].position.y)^2);
+//         console.log('zDistance: '+(object.position.z-spawns[i].position.z)^2);
+//         console.log('distance: '+ distance);
+//         if(distance < 1){
+//             score++;
+//             updateText(String(score)); 
+//             console.log('score: '+score);
+//         }
+//     }
+// }
 
 function onKeyDown(){
     var dampening = .05;
     switch( event.keyCode ) {
         case 65: // a key/left
-        // console.log('camera y rotation'+ camera.rotation.y);
         camera.position.x+=Math.sin(camera.rotation.y - Math.PI/2);
         camera.position.z+=Math.cos(camera.rotation.y - Math.PI/2);
         break;
         case 68: // d key/right
-        // console.log('camera y rotation'+ camera.rotation.y);
         camera.position.x+=Math.sin(camera.rotation.y + Math.PI/2);
         camera.position.z+=Math.cos(camera.rotation.y + Math.PI/2);
         break;
@@ -93,29 +148,26 @@ function onKeyDown(){
         camera.position.z+=Math.cos(camera.rotation.y);
         break;
         case 87: // w key/forwards
-        
-        // console.log('w move start '+camera.position.x+', '+camera.position.z);
         camera.position.x-=Math.sin(camera.rotation.y);
         camera.position.z-=Math.cos(camera.rotation.y);
-        // console.log('w move end '+camera.position.x+', '+camera.position.z);
         break;
         case 37: // left arrow
-        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
-        camera.rotation.y += Math.PI/90;
+        camera.rotation.y += Math.PI/180;
         break;
         case 38: // up arrow
-        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
-        camera.rotation.x += Math.PI/90;
+        console.log('up camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.x += Math.PI/180;
         break;
         case 39: // right arrow
-        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
-        camera.rotation.y -= Math.PI/90;
+        console.log('right camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.y -= Math.PI/180;
         break;
         case 40: // down arrow
-        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
-        camera.rotation.x -= Math.PI/90;
+        console.log('down camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.x -= Math.PI/180;
         break;
-        case 13: // down arrow
+        case 13: // enter
+        console.log('shoot');
         shoot();
         break;
     }
@@ -131,6 +183,10 @@ var animate = function () {
     for(var i = 0; i < spawns.length; i++){
         spawns[i].rotation.x += 0.1;
         spawns[i].rotation.y += 0.1;
+    }
+    for(var i = 0; i < bullets.length; i++){
+        bullets[i].position.add(bullets[i].velocity);
+        // detectCollision(bullets[i]);
     }
     renderer.render(scene, camera);
 };
