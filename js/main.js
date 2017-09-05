d4ca919980a5520eaa9f52ca651e3f10adbf83d7
Nathaniel Sena <nate.sena1@gmail.com>
2017-09-05 10:19:52 -0700

    fixed lateral movement

--- js/main.js
+++ js/main.js
@@ -8,18 +8,35 @@ document.body.appendChild( renderer.domElement );
 document.body.addEventListener( 'keydown', onKeyDown, false );
 
 var spawns = [];
+var bullets = [];
 
-//-------------------------------
-var spriteMap = new THREE.TextureLoader().load( 'img/gun_sight.png' );
+//-----------GUI Elements--------------------
+var loader = new THREE.TextureLoader();
+var spriteMap = loader.load( 'img/gun_sight.png' );
 var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
 var sprite = new THREE.Sprite( spriteMaterial );
-sprite.scale.set(1, 1, 1);
-sprite.position.set(0,0,-5);
+sprite.scale.set(.1, .1, .1);
+sprite.position.set(0,0,-1);
 camera.add(sprite);
 scene.add( camera );
 
 //-------------------------------
+// ambient light to see floor
+scene.add( new THREE.AmbientLight( 0x666666 ) );
 
+//floor
+var groundTexture = loader.load( 'img/grasslight-big.jpg' );
+groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+groundTexture.repeat.set( 25, 25 );
+groundTexture.anisotropy = 16;
+var groundMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, map: groundTexture } );
+var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 1000, 1000 ), groundMaterial );
+mesh.position.y = - 5;
+mesh.rotation.x = - Math.PI / 2;
+scene.add( mesh );
+
+
+//object functions
 function ranPos(x, y, z){
     var coordinates = [];
     var xPos = Math.random() * x;
@@ -47,22 +64,60 @@ function spawnBox() {
     scene.add( cube );
     cube.position.set(posArr[0],posArr[1],posArr[2])
 }
+function shoot(){
+    var geometry = new THREE.SphereGeometry( .05, 8, 8 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+    var bullet = new THREE.Mesh( geometry, material );
+    bullet.position.set(camera.position.x, camera.position.y, camera.position.z - 1);
+    bullets.push(bullet);
+    scene.add(bullet);
+    console.log(camera.position);
+    
+}
 
 function onKeyDown(){
     var dampening = .05;
     switch( event.keyCode ) {
-        case 65: // up
-        camera.position.x -= dampening;
+        case 65: // a key/left
+        // console.log('camera y rotation'+ camera.rotation.y);
+        camera.position.x+=Math.sin(camera.rotation.y - Math.PI/2);
+        camera.position.z+=Math.cos(camera.rotation.y - Math.PI/2);
         break;
-        case 68: // up
-        camera.position.x += dampening;
+        case 68: // d key/right
+        // console.log('camera y rotation'+ camera.rotation.y);
+        camera.position.x+=Math.sin(camera.rotation.y + Math.PI/2);
+        camera.position.z+=Math.cos(camera.rotation.y + Math.PI/2);
         break;
-       case 83: // up
-       camera.position.z += dampening;
-       break;
-       case 87: // down
-       camera.position.z -= dampening;
-       break;
+        case 83: // s key/backward
+        camera.position.x+=Math.sin(camera.rotation.y);
+        camera.position.z+=Math.cos(camera.rotation.y);
+        break;
+        case 87: // w key/forwards
+        
+        // console.log('w move start '+camera.position.x+', '+camera.position.z);
+        camera.position.x-=Math.sin(camera.rotation.y);
+        camera.position.z-=Math.cos(camera.rotation.y);
+        // console.log('w move end '+camera.position.x+', '+camera.position.z);
+        break;
+        case 37: // left arrow
+        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.y += Math.PI/90;
+        break;
+        case 38: // up arrow
+        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.x += Math.PI/90;
+        break;
+        case 39: // right arrow
+        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.y -= Math.PI/90;
+        break;
+        case 40: // down arrow
+        console.log('camera rotation'+ camera.rotation.x+','+camera.rotation.y+','+camera.rotation.z);
+        camera.rotation.x -= Math.PI/90;
+        break;
+        case 13: // down arrow
+        shoot();
+        break;
     }
 }
 
